e1d836ac31351b9d0d31b7d1ca874c92e248cf4c
Nicolas BREUIL <breuil.n@sfeir.com>
2022-12-13 13:34:05 +0100

    D11 ajout du modulo magic

--- D11/Program.cs
+++ D11/Program.cs
@@ -1,14 +1,15 @@
 ﻿using System.Numerics;
-//var inputs = "Monkey 0:\r\n  Starting items: 73, 77\r\n  Operation: new = old * 5\r\n  Test: divisible by 11\r\n    If true: throw to monkey 6\r\n    If false: throw to monkey 5\r\n\r\nMonkey 1:\r\n  Starting items: 57, 88, 80\r\n  Operation: new = old + 5\r\n  Test: divisible by 19\r\n    If true: throw to monkey 6\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 61, 81, 84, 69, 77, 88\r\n  Operation: new = old * 19\r\n  Test: divisible by 5\r\n    If true: throw to monkey 3\r\n    If false: throw to monkey 1\r\n\r\nMonkey 3:\r\n  Starting items: 78, 89, 71, 60, 81, 84, 87, 75\r\n  Operation: new = old + 7\r\n  Test: divisible by 3\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 0\r\n\r\nMonkey 4:\r\n  Starting items: 60, 76, 90, 63, 86, 87, 89\r\n  Operation: new = old + 2\r\n  Test: divisible by 13\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 7\r\n\r\nMonkey 5:\r\n  Starting items: 88\r\n  Operation: new = old + 1\r\n  Test: divisible by 17\r\n    If true: throw to monkey 4\r\n    If false: throw to monkey 7\r\n\r\nMonkey 6:\r\n  Starting items: 84, 98, 78, 85\r\n  Operation: new = old * old\r\n  Test: divisible by 7\r\n    If true: throw to monkey 5\r\n    If false: throw to monkey 4\r\n\r\nMonkey 7:\r\n  Starting items: 98, 89, 78, 73, 71\r\n  Operation: new = old + 4\r\n  Test: divisible by 2\r\n    If true: throw to monkey 3\r\n    If false: throw to monkey 2";
+var inputs = "Monkey 0:\r\n  Starting items: 73, 77\r\n  Operation: new = old * 5\r\n  Test: divisible by 11\r\n    If true: throw to monkey 6\r\n    If false: throw to monkey 5\r\n\r\nMonkey 1:\r\n  Starting items: 57, 88, 80\r\n  Operation: new = old + 5\r\n  Test: divisible by 19\r\n    If true: throw to monkey 6\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 61, 81, 84, 69, 77, 88\r\n  Operation: new = old * 19\r\n  Test: divisible by 5\r\n    If true: throw to monkey 3\r\n    If false: throw to monkey 1\r\n\r\nMonkey 3:\r\n  Starting items: 78, 89, 71, 60, 81, 84, 87, 75\r\n  Operation: new = old + 7\r\n  Test: divisible by 3\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 0\r\n\r\nMonkey 4:\r\n  Starting items: 60, 76, 90, 63, 86, 87, 89\r\n  Operation: new = old + 2\r\n  Test: divisible by 13\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 7\r\n\r\nMonkey 5:\r\n  Starting items: 88\r\n  Operation: new = old + 1\r\n  Test: divisible by 17\r\n    If true: throw to monkey 4\r\n    If false: throw to monkey 7\r\n\r\nMonkey 6:\r\n  Starting items: 84, 98, 78, 85\r\n  Operation: new = old * old\r\n  Test: divisible by 7\r\n    If true: throw to monkey 5\r\n    If false: throw to monkey 4\r\n\r\nMonkey 7:\r\n  Starting items: 98, 89, 78, 73, 71\r\n  Operation: new = old + 4\r\n  Test: divisible by 2\r\n    If true: throw to monkey 3\r\n    If false: throw to monkey 2";
 
-var inputs = "Monkey 0:\r\n  Starting items: 79, 98\r\n  Operation: new = old * 19\r\n  Test: divisible by 23\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 3\r\n\r\nMonkey 1:\r\n  Starting items: 54, 65, 75, 74\r\n  Operation: new = old + 6\r\n  Test: divisible by 19\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 79, 60, 97\r\n  Operation: new = old * old\r\n  Test: divisible by 13\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 3\r\n\r\nMonkey 3:\r\n  Starting items: 74\r\n  Operation: new = old + 3\r\n  Test: divisible by 17\r\n    If true: throw to monkey 0\r\n    If false: throw to monkey 1";
+//var inputs = "Monkey 0:\r\n  Starting items: 79, 98\r\n  Operation: new = old * 19\r\n  Test: divisible by 23\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 3\r\n\r\nMonkey 1:\r\n  Starting items: 54, 65, 75, 74\r\n  Operation: new = old + 6\r\n  Test: divisible by 19\r\n    If true: throw to monkey 2\r\n    If false: throw to monkey 0\r\n\r\nMonkey 2:\r\n  Starting items: 79, 60, 97\r\n  Operation: new = old * old\r\n  Test: divisible by 13\r\n    If true: throw to monkey 1\r\n    If false: throw to monkey 3\r\n\r\nMonkey 3:\r\n  Starting items: 74\r\n  Operation: new = old + 3\r\n  Test: divisible by 17\r\n    If true: throw to monkey 0\r\n    If false: throw to monkey 1";
 
 
 var Monkeys = inputs.Split("\r\n\r\n").Select(raw => new Monkey(raw, 3)).ToList();
+var magicModulo = Monkeys.Aggregate(1ul, (product, monkey) => product * monkey.WorryTester);
 
 foreach (var i in Enumerable.Range(1,20))
 {
-    Monkeys.ForEach(monkey => monkey.EvaluateItemToThrow().ToList().ForEach(ia => Monkeys.First(m => m.MonkeyId == ia.ToMonkeyId).ItemsCarried.Enqueue(ia.item)));
+    Monkeys.ForEach(monkey => monkey.EvaluateItemToThrow(magicModulo).ToList().ForEach(ia => Monkeys.First(m => m.MonkeyId == ia.ToMonkeyId).ItemsCarried.Enqueue(ia.item)));
     //Monkeys.ForEach(monkey => Console.WriteLine(monkey.ToString()));
     //Console.WriteLine("                   ");
 }
@@ -18,11 +19,12 @@ var resultPart1 = Monkeys.OrderByDescending(monkey => monkey.NumberOfItemAnalize
 Console.WriteLine(resultPart1);
 
 var Monkeys2 = inputs.Split("\r\n\r\n").Select(raw => new Monkey(raw, 1)).ToList();
+var magicModulo2 = Monkeys2.Aggregate(1ul, (product, monkey) => product * monkey.WorryTester);
 
 foreach (var i in Enumerable.Range(1, 10_000))
 {
-    Monkeys2.ForEach(monkey => monkey.EvaluateItemToThrow().ToList().ForEach(ia => Monkeys2.First(m => m.MonkeyId == ia.ToMonkeyId).ItemsCarried.Enqueue(ia.item)));
-    Console.WriteLine($"{i}         ");
+    Monkeys2.ForEach(monkey => monkey.EvaluateItemToThrow(magicModulo2).ToList().ForEach(ia => Monkeys2.First(m => m.MonkeyId == ia.ToMonkeyId).ItemsCarried.Enqueue(ia.item)));
+    //Console.WriteLine($"{i}         ");
     //Console.WriteLine($"          {i}         ");
     //Monkeys.ForEach(monkey => Console.WriteLine(monkey.ToString()));
 }
@@ -35,29 +37,29 @@ internal class Monkey
 {
     internal readonly int MonkeyId;
     internal Queue<Item> ItemsCarried;
+    internal readonly ulong WorryTester;
     internal long NumberOfItemAnalized = 0;
 
-    private readonly int WorryTester;
-    private readonly int DevWorryLevel;
+    private readonly ulong DevWorryLevel;
     private readonly int MonkeyIdToSendIfTrue;
     private readonly int MonkeyIdToSendIfFalse;
-    private readonly Func<Item, BigInteger> Operation;
+    private readonly Func<Item, ulong> Operation;
 
-    internal Monkey(string rawMonkey, int devWorryLevel)
+    internal Monkey(string rawMonkey, ulong devWorryLevel)
     {
         var datas = rawMonkey.Split("\r\n");
 
         MonkeyId = int.Parse(datas[0].Replace("Monkey ", string.Empty).Replace(":", string.Empty));
         ItemsCarried = new Queue<Item>(datas[1].Replace("  Starting items: ", string.Empty).Split(", ").Select(i => new Item(ulong.Parse(i))));
         Operation = ParseOperation(datas[2].Replace("  Operation: new = ", string.Empty));
-        WorryTester = int.Parse(datas[3].Replace("  Test: divisible by ", string.Empty));
+        WorryTester = ulong.Parse(datas[3].Replace("  Test: divisible by ", string.Empty));
         MonkeyIdToSendIfTrue = int.Parse(datas[4].Replace("    If true: throw to monkey ", string.Empty));
         MonkeyIdToSendIfFalse = int.Parse(datas[5].Replace("    If false: throw to monkey ", string.Empty));
 
         DevWorryLevel = devWorryLevel;
     }
 
-    internal IEnumerable<ItemThrow> EvaluateItemToThrow()
+    internal IEnumerable<ItemThrow> EvaluateItemToThrow(ulong modulo)
     {
         var toThrow = new List<ItemThrow>();
 
@@ -67,25 +69,25 @@ internal class Monkey
         {
             var item = ItemsCarried.Dequeue();
 
-            EvaluateNewWorryLevel(item);
+            EvaluateNewWorryLevel(item, modulo);
 
             yield return item.Value % WorryTester == 0 ? new ItemThrow(item, MonkeyIdToSendIfTrue) : new ItemThrow(item, MonkeyIdToSendIfFalse);
         }
     }
 
-    private void EvaluateNewWorryLevel(Item item)
+    private void EvaluateNewWorryLevel(Item item, ulong modulo)
     {
         if (DevWorryLevel > 1)
-            item.Value = (BigInteger)Math.Round((decimal)(Operation(item) / DevWorryLevel), 0);
+            item.Value = (ulong)Math.Round((decimal)(Operation(item) / DevWorryLevel), 0) % modulo;
         else
-            item.Value = Operation(item);
+            item.Value = Operation(item) % modulo;
     }
 
-    private Func<Item, BigInteger> ParseOperation(string operation)
+    private Func<Item, ulong> ParseOperation(string operation)
         => (item) =>
         {
             var exp = operation.Split(" ");
-            var right = exp[2] == "old" ? item.Value : BigInteger.Parse(exp[2]);
+            var right = exp[2] == "old" ? item.Value : ulong.Parse(exp[2]);
             switch (exp[1])
             {
                 case "+":
@@ -103,8 +105,8 @@ internal class Monkey
 
 internal class Item
 {
-    internal BigInteger Value;
-    internal Item(BigInteger value)
+    internal ulong Value;
+    internal Item(ulong value)
     {
         Value = value;
     }
